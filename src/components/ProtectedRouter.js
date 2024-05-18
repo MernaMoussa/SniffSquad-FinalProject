ef1810c0d8ff296a29bcb571d9731cb661615c14
Merna Moussa <mernahelmy.m@gmail.com>
2024-05-18 17:03:31 +0200

    change auth in protectedroute

--- src/components/ProtectedRouter.js
+++ src/components/ProtectedRouter.js
@@ -3,14 +3,13 @@ import { Navigate } from "react-router-dom";
 import { UserContext } from "../context/UserProvider";
 
 const ProtectedRoute = ({ children }) => {
-  /*const { isAuth, checkedAuth } = useContext(UserContext);
+  const { isAuth, checkedAuth } = useContext(UserContext);
   console.log(isAuth);
   if (!checkedAuth) {
     return null;
-  }*/
-  const accessToken = document.cookie.includes("access_token");
+  }
 
-  if (!accessToken) {
+  if (!isAuth) {
     return <Navigate to="/login" />;
   }
 
