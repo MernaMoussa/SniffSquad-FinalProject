7c13799033a69dc45745b8ac6c84311689b54af3
Merna Moussa <mernahelmy.m@gmail.com>
2024-05-26 13:54:01 +0200

    adjust how to get dog' photo url

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "@fontsource/roboto/300.css";
@@ -10,9 +10,9 @@ import "@fontsource/roboto/700.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
 
 reportWebVitals();
